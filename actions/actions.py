5dbee2c81540fbfc5ec2280842cfddf5751a46bf
Rohit Patil <rohit4change@yahoo.in>
2021-12-03 11:02:17 -0500

    Deadline intent added

--- actions/actions.py
+++ actions/actions.py
@@ -65,3 +65,34 @@ class GetHolidays(Action):
         dispatcher.utter_message(text="Hello " + name + "! There is no Class on: \n" + holiday_df.to_string())
 
         return []
+
+class GetDeadlines(Action):
+
+    def name(self) -> Text:
+        return "action_get_deadlines"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("name")
+        assignmentNum = tracker.get_slot("assignmentNum")
+        print(assignmentNum)
+        # print current path
+        print(os.getcwd())
+
+        # read csv file
+        deadlines = pd.read_csv('data/deadlines.csv')
+        deadlines.columns = ['No', 'Assessment', 'Due Date (11:59 PM EST of the day)']
+        print(deadlines.columns)
+        # check if assignmentNum is a number
+        if assignmentNum.isdigit():
+            if int(assignmentNum) > 0:
+                subset_df = deadlines[deadlines['Assessment'].str.contains("Assignment "+assignmentNum) == True]
+                dispatcher.utter_message(text="Hello " + name + "! The deadline for Assignment " + assignmentNum + " is: \n" + subset_df.to_string())
+            else:
+                dispatcher.utter_message(text="Hello " + name + "! The deadlines are as follows: \n" + deadlines.to_string())
+        else:
+            dispatcher.utter_message(text="Hello " + name + "! The deadlines are as follows: \n" + deadlines.to_string())
+
+        return []
