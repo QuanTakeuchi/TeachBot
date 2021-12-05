f1763af2e9ac78df92713620b844be2ec1af36a3
Rohit Patil <rohit4change@yahoo.in>
2021-12-04 22:23:49 -0500

    some more updates

--- actions/actions.py
+++ actions/actions.py
@@ -15,6 +15,88 @@ import pandas as pd
 
 import os
 
+class GetBooks(Action):
+
+    def name(self) -> Text:
+        return "action_get_books"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("name")
+        # print current path
+        print(os.getcwd())
+
+        # read json file into pandas dataframe
+        df = pd.read_csv('data/books.csv')
+
+        # convert pandaas dataframe to formatted string
+
+        dispatcher.utter_message(text="Hello ! Your schedule is: \n" + df.to_string())
+
+        return []
+
+
+class GetJurasky(Action):
+
+    def name(self) -> Text:
+        return "action_get_jurafsky"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # read json file into pandas dataframe
+        df = pd.read_csv('data/Jurafsky.csv')
+
+        # convert pandaas dataframe to formatted string
+
+        dispatcher.utter_message(text="Here are the notes \n" + df.to_string())
+
+        return []
+
+class GetBird(Action):
+
+    def name(self) -> Text:
+        return "action_get_bird"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # read json file into pandas dataframe
+        df = pd.read_csv('data/Bird.csv')
+
+        # convert pandaas dataframe to formatted string
+
+        dispatcher.utter_message(text="Here are the notes \n" + df.to_string())
+
+        return []
+
+class GetGrading(Action):
+
+    def name(self) -> Text:
+        return "action_get_grading"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # print current path
+        print(os.getcwd())
+
+        # read text file into an array
+        with open('data/Grades.txt', 'r') as myfile:
+            data = myfile.readlines()
+
+        # convert pandaas dataframe to formatted string
+
+
+        dispatcher.utter_message(text="Hello ! Your Grading is: \n" + "".join(data))
+
+        return []
+
 class GetSchedule(Action):
 
     def name(self) -> Text:
@@ -37,6 +119,7 @@ class GetSchedule(Action):
         dispatcher.utter_message(text="Hello " + name + "! Your schedule is: \n" + schedule.to_string())
 
         return []
+
 
 class GetHolidays(Action):
 
@@ -95,7 +178,7 @@ class GetDeadlines(Action):
                 dispatcher.utter_message(text="Hello " + name + "! The deadlines are as follows: \n" + deadlines.to_string())
                 return []
         else:
-            subset_df = deadlines[deadlines['Assessment'].str.contains("assignmentNum") == True]
+            subset_df = deadlines[deadlines['Assessment'].str.contains(assignmentNum) == True]
             dispatcher.utter_message(text="Hello " + name + "! The deadlines are as follows: \n" + subset_df.to_string())
                           
         dispatcher.utter_message(text="Hello " + name + "! The assignment details are incorrect \n" )   
@@ -117,17 +200,71 @@ class GetNotes(Action):
 
         # read csv file
         notes = pd.read_csv('data/Notes.csv')
+        jurafsky = pd.read_csv('data/Jurafsky.csv')
+        bird = pd.read_csv('data/Bird.csv')
         notes.columns = ['Week', 'Chapter', 'Url']
+        jurafsky.columns = ['Week', 'Chapter', 'Url']
+        bird.columns = ['Week', 'Chapter', 'Url']
+
         print(notes.columns)
         # check if assignmentNum is a number
+
+        subset_df = pd.DataFrame(columns=['Week', 'Chapter', 'Url'])
+        
         for i in range(len(notes)):
             # check if notes['Chapter'] contains the string "chapterName" and filter out the rows
             if chapterName.lower() in notes['Chapter'][i].lower():
-                dispatcher.utter_message(text="Hello " + name + "! The notes for topic " + chapterName + " could be found here: \n" + notes['Url'][i])
-                return []
-        dispatcher.utter_message(text="Hello " + name + "! There are no notes for the topic " + chapterName + ".")
+                # subset current row into a dataframe
+                subset_df = subset_df.append(notes.iloc[i])
+                # dispatcher.utter_message(text="Hello " + name + "! The notes for topic " + chapterName + " could be found here: \n" + subset_df.to_string())
+            if chapterName.lower() in jurafsky['Chapter'][i].lower():
+                # subset current row into a dataframe
+                subset_df = subset_df.append(jurafsky.iloc[i])    
+            if chapterName.lower() in bird['Chapter'][i].lower():
+                subset_df = subset_df.append(bird.iloc[i])
+        if len(subset_df) == 0:
+            dispatcher.utter_message(text="Hello " + name + "! The notes for topic " + chapterName + " could not be found \n")
+            dispatcher.utter_message(text="Showing results for all the notes \n" + subset_df.to_string())
+        else:
+            dispatcher.utter_message(text="Hello " + name + "! The notes for the given topics are as follows:  \n" + subset_df.to_string())
 
         return []
+
+
+class GetNotesTopic(Action):
+
+    def name(self) -> Text:
+        return "action_show_note_topics"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # read csv file
+        print("reading notes")
+        notes = pd.read_csv('data/Notes.csv')
+        print("reading jurafsky")
+        jurafsky = pd.read_csv('data/Jurafsky.csv')
+        print("reading bird")
+        bird = pd.read_csv('data/Bird.csv')
+        notes.columns = ['Week', 'Chapter', 'Url']
+        jurafsky.columns = ['Week', 'Chapter', 'Url']
+        bird.columns = ['Week', 'Chapter', 'Url']
+
+        print(notes.columns)
+        # check if assignmentNum is a number
+
+        # Extract only chapter column from the dataframe
+        notes_chapters = notes['Chapter']
+        jurafsky_chapters = jurafsky['Chapter']
+        bird_chapters = bird['Chapter']
+
+        dispatcher.utter_message(text="Hello! The following topics are available from class notes: \n" + notes_chapters.to_string())
+        dispatcher.utter_message(text="Hello! The following topics are available from jurafsky: \n" + jurafsky_chapters.to_string())
+        dispatcher.utter_message(text="Hello! The following topics are available from bird: \n" + bird_chapters.to_string())
+        
+        return []
+
 
 class GetMiniTalk(Action):
 
